07f4d39a6b17eaaac5a0f8c1617483c149107a30
Vitaliy070516 <v_goroshko@ukr.net>
2016-12-09 14:22:30 +0200

    Update step1.php

--- step1.php
+++ step1.php
@@ -2,7 +2,9 @@
 
 // This recursive function can be used for counting factorial from some number
 function f($x) {
-	// If variable $x doesn't equal "0" it's multiplied on the function f with argument $x-1. This behavior of calling function inside of the same function repeats until variable $x equals "0". Then the function f returns "1" and all members of expression are multiplied and the result is returned
+// If variable $x doesn't equal "0" it's multiplied on the function f with argument $x-1. This behavior of calling function inside of the
+// same function repeats until variable $x equals "0". Then the function f returns "1" and all members of expression are multiplied and 
+// the result is returned
 	if ($x === 0) return 1;
 	else return $x * f($x-1);
 }
