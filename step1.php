493c1147d6f88dc504076ce7f311006beb8fafd1
Vitaliy070516 <v_goroshko@ukr.net>
2016-12-13 23:10:19 +0200

    added Exception for a case of negative number

--- step1.php
+++ step1.php
@@ -5,8 +5,13 @@ function f($x) {
 // If variable $x doesn't equal "0" it's multiplied on the function f with argument $x-1. This behavior of calling function inside of the
 // same function repeats until variable $x equals "0". Then the function f returns "1" and all members of expression are multiplied and 
 // the result is returned
-	if ($x === 0) return 1;
-	else return $x * f($x-1);
+	try {
+		if ($x < 0) throw new Exception('Error! Invalid argument! You have used a negative number!');
+		if ($x === 0) return 1;
+		else return $x * f($x-1);
+	} catch(Exception $e) {
+		echo $e->getMessage();
+	}
 }
 // For example we can count factorial from number 7
 echo f(7);
